5168a687f4bc02ac407e114936d929befa8f4636
Denis <denisgridin6@gmail.com>
2019-04-05 12:41:30 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,1 @@
+var menu=document.getElementById('submenu');var open=document.getElementById('hamburger');var close=document.getElementById('close');function openMenu(){open.style.display="none";close.style.display="block";menu.style.display="block";}function closeMenu(){open.style.display="block";close.style.display="none";menu.style.display="none";}
